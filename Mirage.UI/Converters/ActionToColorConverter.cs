8844f2e530f6100f5eaf4709acabd403f9ecb9b1
Farey13 <demha.faruhan@gmail.com>
2026-01-16 10:55:47 +0500

    Color-code audit actions in log view for clarity
    Added ActionToColorConverter to map audit actions to colors in the UI (green for CREATE, orange for UPDATE, red for DEACTIVATE, gray for others). Updated AuditLogView.xaml to use a DataGridTemplateColumn for the "Action" field, displaying the action type in bold and colored text for improved readability. Registered the converter as a resource in the view.

---
+++ Mirage.UI/Converters/ActionToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Mirage.UI.Converters;
+
+public class ActionToColorConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        // Normalize the action string to handle case sensitivity
+        string action = value?.ToString()?.ToUpper() ?? "";
+
+        return action switch
+        {
+            "CREATE" => Brushes.Green,      // New data added
+            "UPDATE" => Brushes.Orange,     // Existing data changed
+            "DEACTIVATE" => Brushes.Red,    // Item was soft-deleted
+            _ => Brushes.Gray               // Default for any other actions
+        };
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        => throw new NotImplementedException();
+}
